1411a04626cf1c14de79a00d8789c0136fb68208
Hwang-YoonGyu <dotheji11@naver.com>
2022-06-22 16:32:35 +0900

    2022-06-22 16:32

---
+++ HansungRentSystem_iOS/HansungRentSystem_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HansungRentSystem_iOS
+//
+//  Created by 황윤규 on 2022/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
