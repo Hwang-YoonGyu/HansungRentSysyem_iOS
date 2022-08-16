eda2f27612c63649d969395991027629055d815d
jinwoongCho <100175892+jinwoongCho@users.noreply.github.com>
2022-08-16 18:09:37 +0900

    기자재 상세정보

---
+++ HansungRentSystem_iOS/HansungRentSystem_iOS/DetailInfo1.swift
@@ -0,0 +1,19 @@
+//
+//  DetailInfo1.swift
+//  HansungRentSystem_iOS
+//
+//  Created by 조진웅 on 2022/08/16.
+//
+
+import UIKit
+
+class DetailInfo1: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    @IBAction func backBtn(_ sender: UIButton) {
+        navigationController?.popViewController(animated: true)
+    }
+}
