d3d002d4f028056f52ce76ab4abc0ac65d9a24e6
Hwang-YoonGyu <dotheji11@naver.com>
2022-09-02 14:46:48 +0900

    빌드에러 잡는중

--- HansungRentSystem_iOS/HansungRentSystem_iOS/List.swift
+++ HansungRentSystem_iOS/HansungRentSystem_iOS/List.swift
@@ -6,7 +6,6 @@
 //
 
 import Foundation
-import ViewAnimator
 import UIKit
 class List : UIViewController, UITableViewDataSource, UITableViewDelegate {
     
--- HansungRentSystem_iOS/Pods/Target Support Files/Pods-HansungRentSystem_iOS/Pods-HansungRentSystem_iOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_HansungRentSystem_iOS : NSObject
-@end
-@implementation PodsDummy_Pods_HansungRentSystem_iOS
-@end
